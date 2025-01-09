d292fef80043522e50b0e2ebe868d947f82f7ac9
sfpatten <pattensf0@gmail.com>
2025-01-09 09:47:08 +0000

    Un-revert fix switch-case fallthrough and accidentally destroy all commit history in the process

--- battle.js
+++ battle.js
@@ -116,12 +116,16 @@ class Battle {
         switch (this.save) {
             case "str":
                 damage -= game.player.rollStr();
+                break;
             case "dex":
                 damage -= game.player.rollDex();
+                break;
             case "int":
                 damage -= game.player.rollInt();
+                break;
             case "cha":
                 damage -= game.player.rollCha();
+                break;
             default:
                 damage -= game.player.rollArm();
         }
