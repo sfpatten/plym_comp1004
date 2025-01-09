befd772e3230da63c24f60688e293f893ee5bdb9
sfpatten <pattensf0@gmail.com>
2025-01-09 09:45:04 +0000

    Set up Game and Level classes; Create 2D array levelGrid

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 // document.getElementById("temp").innerHTML = "JS is working.";
+
+class Level {
+    constructor(size) {
+        this.size = size;
+
+        // set up the level grid
+        this.levelGrid = new Array(size); // make the thing
+            // populate each first dimension with its own new array
+        for (let i = 0; i < size; i++) {
+            this.levelGrid[i] = new Array(size);
+        }
+            // fill each tile with 0
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                this.levelGrid[i][j] = 0;
+            }
+        }
+
+    }
+}
+
+class Game {
+    constructor() {
+        this.botNumber = 0;
+        this.overworld = new Level(50);
+    }
+}
+
+let game = new Game();
